c1b1625853251a796b6175af329b4a803e26d553
sponsbob10 <rezaadnan62@gmail.com>
2020-02-29 10:39:49 +0700

    projek spermato

---
+++ Spermatophyta/app/src/main/java/com/amazing/spermatophyta/silabus/ProfileDsnActivity.java
@@ -0,0 +1,16 @@
+package com.amazing.spermatophyta.silabus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.amazing.spermatophyta.R;
+
+public class ProfileDsnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_dsn);
+    }
+}
