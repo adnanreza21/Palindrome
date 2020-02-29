6ed745a257105e9293032e5ad29592056c0f6fe6
sponsbob10 <rezaadnan62@gmail.com>
2020-02-29 11:20:48 +0700

    projek spermato

--- Spermatophyta/app/src/main/java/com/amazing/spermatophyta/ProfileActivity.java
+++ Spermatophyta/app/src/main/java/com/amazing/spermatophyta/ProfileActivity.java
@@ -1,14 +1,39 @@
 package com.amazing.spermatophyta;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.amazing.spermatophyta.materi.MlimaActivity;
+import com.amazing.spermatophyta.silabus.ProfileDsnActivity;
+import com.amazing.spermatophyta.silabus.ProfileMhsActivity;
 
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private CardView Card1,Card2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Card1 = (CardView) findViewById(R.id.card1);
+        Card2 = (CardView) findViewById(R.id.card2);
+
+        Card1.setOnClickListener(this);
+        Card2.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i ;
+
+        switch (v.getId()){
+            case R.id.card1 : i = new Intent(this, ProfileDsnActivity.class);startActivity(i);break;
+            case R.id.card2 : i = new Intent(this, ProfileMhsActivity.class);startActivity(i);break;
+            default:break;
+        }
     }
 }
