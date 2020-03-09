7f85f89d7549ccb1051ee61eea630e37ecb88442
sponsbob10 <rezaadnan62@gmail.com>
2020-03-09 12:24:49 +0700

    projek spermato

---
+++ Spermatophyta/app/src/main/java/com/amazing/spermatophyta/materi/MateriOpenActivity.java
@@ -0,0 +1,16 @@
+package com.amazing.spermatophyta.materi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.amazing.spermatophyta.R;
+
+public class MateriOpenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_materi_open);
+    }
+}
