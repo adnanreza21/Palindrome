6befc9b93620c43630ef539d3eb4ed601d0c06e4
sponsbob10 <rezaadnan62@gmail.com>
2020-03-11 08:50:41 +0700

    projek

---
+++ MyApplicationGigih/app/src/main/java/com/amazing/myapplicationgigih/materi/Materi5Activity.java
@@ -0,0 +1,16 @@
+package com.amazing.myapplicationgigih.materi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.amazing.myapplicationgigih.R;
+
+public class Materi5Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_materi5);
+    }
+}
