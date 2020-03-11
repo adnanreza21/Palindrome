cd2b45217f1b027cae02700487af944529ef8d02
sponsbob10 <rezaadnan62@gmail.com>
2020-03-11 10:30:54 +0700

    projek

--- MyApplicationGigih/app/src/main/java/com/amazing/myapplicationgigih/SilabusActivity.java
+++ MyApplicationGigih/app/src/main/java/com/amazing/myapplicationgigih/SilabusActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Silabus extends AppCompatActivity {
+public class SilabusActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
