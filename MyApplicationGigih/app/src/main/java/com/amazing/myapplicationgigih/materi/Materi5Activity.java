838861227b0668c4927703f81e3c2bc6d2de5329
sponsbob10 <rezaadnan62@gmail.com>
2020-03-16 02:13:30 +0700

    projek

--- MyApplicationGigih/app/src/main/java/com/amazing/myapplicationgigih/materi/Materi5Activity.java
+++ MyApplicationGigih/app/src/main/java/com/amazing/myapplicationgigih/materi/Materi5Activity.java
@@ -20,7 +20,7 @@ public class Materi5Activity extends AppCompatActivity {
         // TAMBAHKAN INI
         GambarGif1 = (ImageView) findViewById(R.id.gifsatu);
         GambarGif2 = (ImageView) findViewById(R.id.gifdua);
-        GambarGif2 = (ImageView) findViewById(R.id.giftiga);
+        GambarGif3 = (ImageView) findViewById(R.id.giftiga);
 
         Glide.with(Materi5Activity.this)
                 // LOAD URL DARI LOKAL DRAWABLE
