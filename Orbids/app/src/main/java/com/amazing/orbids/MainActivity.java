20549631a294d90a77073e4208bc68af881bf8b9
sponsbob10 <rezaadnan62@gmail.com>
2020-10-09 20:13:20 +0700

    update

---
+++ Orbids/app/src/main/java/com/amazing/orbids/MainActivity.java
@@ -0,0 +1,14 @@
+package com.amazing.orbids;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
